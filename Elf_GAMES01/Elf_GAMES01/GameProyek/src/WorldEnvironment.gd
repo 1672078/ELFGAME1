20a4d4bce4e235da0c4e09c9fb62978d36e77ee6
Samuel Kristianto Dea <it1672078@student.it.maranatha.edu>
2022-06-16 06:11:05 +0700

    update

--- Elf_GAMES01/Elf_GAMES01/GameProyek/src/WorldEnvironment.gd
+++ Elf_GAMES01/Elf_GAMES01/GameProyek/src/WorldEnvironment.gd
@@ -4,4 +4,4 @@ func _ready():
 	GlobalSetting.connect("brightness_updated", self, "_on_brightness_updated")
 
 func _on_brightness_updated(value):
-	environment.adjustment_brightness == value
+	environment.adjustment_brightness = value
